85d9f7907275e48d43d535a7ab5c3409ed8d17f1
Meiqiu Wang <mewan@microsoft.com>
2022-12-26 12:35:05 -0800

    cleanup HelloWorld

--- HelperLib/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace HelperLib;
-public class Class1
-{
-
-}
--- TestUI/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.Write("please enter your first name: ");
-string firstName = Console.ReadLine();
-
-System.Console.WriteLine($"Hello {firstName}");
